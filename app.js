053ea62baa6a71d2942a3021944470112db73a26
Jeff McDonald <jwmcdonald85@gmail.com>
2023-11-11 13:10:01 -0500

    landscaper: user can use teeth to cut grass

---
+++ app.js
@@ -0,0 +1,12 @@
+
+console.log("Working outside!");
+
+// Parameters:
+// Working as a landscaper
+// Starting tool is teeth, earn $1/day, unlimited uses
+
+
+const tools = [
+    {name: "teeth", revenue: 1, cost: 0},
+    {}
+]
